187cbea560af54af4818bb2be4043a223e866f5e
Delightsome Asolo <delightsomeasolo@gmail.com>
2025-05-16 20:36:46 +0100

    Products, routers and controllers

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
 
 const router = Router();
-router.get("/", (req, res) => {
-  res.send("The list of products.");
-});
 
-router.get("/:id", (req, res) => {
-  console.log(req.params);
-  res.send("A product");
-});
+import {
+  listProduct,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "./productsController";
 
-router.post("/", (req, res) => {
-  res.send("New product created!");
-});
+router.get("/", listProduct);
+
+router.get("/:id", getProductById);
+
+router.post("/", createProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
 
 export default router;
