3ea27cf26333ffa245742c18d09135d6abd57df8
Delightsome Asolo <delightsomeasolo@gmail.com>
2025-05-20 10:17:20 +0100

    package.json set up + drizzle ORM

---
+++ api/src/db/productSchema.ts
@@ -0,0 +1,15 @@
+import {
+  integer,
+  pgTable,
+  varchar,
+  text,
+  doublePrecision,
+} from 'drizzle-orm/pg-core';
+
+export const productsTable = pgTable('products', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar('name', { length: 255 }).notNull(),
+  description: text('description'),
+  image: varchar('image', { length: 255 }),
+  price: doublePrecision().notNull(),
+});
