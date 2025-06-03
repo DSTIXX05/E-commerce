e54cc42bac004af5101f740c26e3efe429619f3f
Delightsome Asolo <delightsomeasolo@gmail.com>
2025-06-03 21:20:02 +0100

    db fixed

---
+++ api/src/db/usersSchema.ts
@@ -0,0 +1,11 @@
+import { integer, varchar, text, pgTable } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  role: varchar({ length: 255 }).notNull().default('user'),
+
+  name: varchar({ length: 255 }),
+  address: text(),
+});
